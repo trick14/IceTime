8802f27375faa307ba7b3f0828ac41bc57a7fbf6
Byungki Han <rhan@surfline.com>
2019-03-28 21:36:48 -0700

    Initial Commit

---
+++ IceTime/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IceTime
+//
+//  Created by Ryan Han on 3/28/19.
+//  Copyright © 2019 Ryan Han. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
